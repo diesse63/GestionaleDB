fef704c9a89173ba38270d36b17e69d0f87c81a6
Diesse63 <danielesilli@gmail.com>
2026-01-13 07:10:51 +0100

    Aggiunto sistema di Login, Ruoli Admin/User e API complete

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // Il tuo URL aggiornato
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxI_CAkKZJSoyvN44xiIkCsodqcWKFw-Ix2iZjpD0X1tla78EFmsCvMU1B4JFCEX078iA/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwrgW083UZhqnl9x5SQyVT6SNzv3wnp88WKkeEIO8RHAmxKwAsl10HEVsy-A91BqCzArw/exec",
     APP_NAME: "Gestionale Enti",
     VERSION: "1.1.0"
 };
